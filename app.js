4dacf0872b47b06494e51371cca08c9b5e78d4d7
Christian Buco <christianbuco17@gmail.com>
2017-03-06 08:43:35 -1000

    obj

--- app.js
+++ app.js
@@ -4,22 +4,28 @@
 */
 function accessObjectInArray(arr){
 	//Print the parameter being passed through
-
+	console.log(arr);
 	//How can we tell what type of data this is?
 	//By it's notation, the top level is bracket notation, therefore the parameter is an array
 
 	//What methods can we use for this item?
 	//Array methods
-
+	console.log(arr.length);
 	//Access and print the first object in the parameter 
-
+	console.log(arr[0]);
 	//Access and print the third object in the parameter
-
+	console.log(arr[2]);
 	//Iterate through the array to print all the objects in the parameter
-
-		//Can you concatanete a string to an object?
-
-		//Iterate through the key:value pairings and print all the keys and their values
+	//Can you concatanete a string to an object?
+	// No
+	for (var i = 0; i < arr.length; i++){
+		console.log('app.js line 21');
+		console.log(arr[i]);
+	//Iterate through the key:value pairings and print all the keys and their values
+	for (var key in arr[i]){
+		console.log(key);
+	}
+	}
 
 	//Clean up your code by creating variable names and setting their values
 }
@@ -50,13 +56,25 @@ function accessArrayInObject(obj){
 	//Is the parameter an array or an object?
 
 	//Print the paramter being passed through the function
-
+	console.log(obj);
 	//Iterate through the object
-
+	for(var key in obj){
+		console.log("key")
+		console.log(key);
+		console.log("\nvalues");
+		console.log(obj[key]);
+		console.log("\n")
 	//Create a conditional to check if the key is "cars"
+		if (key === "cars"){
+			console.log("Inside conditional for 'cars'");
+			console.log(obj[key]);
+		for(var carValues in obj[key]){
+			console.log(obj[key][carValues]);
+		}
+		}
+	}
 
 	//Iterate through and print the values in the key "cars"
-
 	//Iterate through and print the key value pairs for each object in the array "toppings"
 
 	//Clean up your code by creating variable names and setting their values
